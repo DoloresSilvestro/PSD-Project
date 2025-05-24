077257888e5c2ef73b27f86baf6d0b7814724f6b
DoloresSilvestro <dolores.silvestro05@gmail.com>
2025-05-24 11:05:51 +0200

    descrizione del modulo lezioni.h -Dolores

---
+++ lezioni.h
@@ -0,0 +1,10 @@
+/*
+La struttura Lezione rappresenta un singolo nodo di una lista collegata che gestisce le lezioni disponibili in palestra.
+Gli elementi principali sono:
+	id: identificativo univoco della lezione;
+	nome: nome della lezione (es. "Yoga", "Pilates");
+	giorno: giorno della settimana in cui si tiene la lezione;
+	durata: durata della lezione espressa in minuti;
+	maxPartecipanti: numero massimo di partecipanti consentiti per la lezione;
+	nodoNext: puntatore al nodo successivo della lista, per gestire dinamicamente l'elenco delle lezioni.
+*/
