7277e2f21dedd2256ce92c1058cb87629323668c
DoloresSilvestro <dolores.silvestro05@gmail.com>
2025-05-24 12:26:36 +0200

    caricamento funzione CreaLezione nel modulo lezioni.c -Dolores

--- lezioni.c
+++ lezioni.c
@@ -25,3 +25,18 @@ Post-condizioni:
   - Se nome è NULL o maxPartecipanti <= 0, la funzione restituisce NULL.
   - Se l’allocazione fallisce, viene stampato un messaggio di errore e la funzione restituisce NULL.
 */
+
+Lezione* creaLezione(int id, const char* nome, const char* giorno, int durata, int maxPrenotazioni) {
+        Lezione* nuovaLezione = calloc(1,sizeof(Lezione));
+        if (!nuovaLezione) return NULL;
+
+            nuovaLezione->id = id;
+            strcpy(nuovaLezione->nome, nome);
+            strcpy(nuovaLezione->giorno, giorno);
+            nuovaLezione->durata = durata;
+            nuovaLezione->maxPrenotazioni = maxPrenotazioni;
+            nuovaLezione->partecipantiAttuali = 0;
+            nuovaLezione->nodoNext = NULL;
+
+        return nuovaLezione;
+}
